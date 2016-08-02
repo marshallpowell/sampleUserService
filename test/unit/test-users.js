7d8e933ba1a8aebf2ef44a08779fa18dd01b0711
marshallpowell <marshallpowell@gmail.com>
2016-08-02 09:58:59 -0400

    updated test

--- test/unit/test-users.js
+++ test/unit/test-users.js
@@ -1,36 +1,48 @@
 'use strict';
 var sinon = require('sinon');
-var proxyquire = require('proxyquire');
-var request = require('supertest');
+var proxyquire = require('proxyquire').noCallThru();
+var supertest = require('supertest');
 var expect = require('chai').expect;
 
+
 var Bluebird        = require('bluebird');
-var httpStub = sinon.stub();
+var dbStub = sinon.stub();
 
 describe('GET /users', function(){
-
+  var request, app, usersRoute;
   beforeEach(function(){
-    httpStub = sinon.stub();
-    request = proxyquire('lib/users', {
-      'request' : {
-        get : httpStub
+
+    dbStub = sinon.stub();
+    app = require('express')();
+    usersRoute = proxyquire('lib/users', {
+      'fh-mbaas-api' : {
+        db : dbStub
       }
     });
+
+
+    app.use('/users',usersRoute());
+    request = supertest(app)
   });
 
-    it('call should return a list of users', function(){
+    it('call should return a list of users', function(done){
 
-        httpStub.yields(null, {
+        dbStub.yields(null, {
           count : 2,
           list : [{},{}]
         });
 
+        console.log('getting ready to make request');
         request.get('/users')
         .expect(200)
         .end(function(err, res){
+          console.log('body list: ' + JSON.stringify(res.body));
+          console.log(dbStub.called);
           expect(err).to.not.exist;
           expect(res.body.list).to.have.length(2);
-        })
+          done();
+
+        });
 
     });
 
